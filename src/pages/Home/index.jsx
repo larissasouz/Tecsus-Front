8a137e7807607bd2fdf6866a428c3fcdbcdc4f53
Noite <wallacehonorato67@gmail.com>
2024-04-08 19:34:23 -0300

    <feat> BT-17 criacao de rotas

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import {useState, } from 'react'
+import { CiUser } from "react-icons/ci";
+
+export default function Home(){
+
+        return(
+        <div className="container-center">
+            <h1>PAGINA HOME</h1>
+        </div>
+    )
+}
