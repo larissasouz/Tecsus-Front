d8b0de0f2ff463f9fe364aa865b614770b8c7d83
Noite <wallacehonorato67@gmail.com>
2024-04-08 21:59:08 -0300

    <feat> BT-67 configuracao do jest e sonar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import RoutesApp from './routes/index.jsx'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <BrowserRouter>
+        <ToastContainer autoClose={3000}/>
         <RoutesApp/>
     </BrowserRouter>
   );
