eccacfdae841fadf9ae223c55147941e3a9bd0e3
Noite <wallacehonorato67@gmail.com>
2024-04-11 22:29:48 -0300

    <feat> BT-70 ajuste sidebar

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,18 @@
-import {useState, } from 'react'
-import { CiUser } from "react-icons/ci";
+import Sidebar from '../../components/sidebar/sidebar'
+import Title from '../../components/title/title'
+import { BsBarChart }  from "react-icons/bs";
+
 
 export default function Dashboard(){
 
         return(
-        <div className="container-center">
-            <h1>PAGINA HOME</h1>
+        <div>
+            <Sidebar/>
+            <main className='page-container'>
+                <Title name={"Dashboard"}>
+                    <BsBarChart size={25} />
+                </Title>
+            </main>
         </div>
     )
 }
