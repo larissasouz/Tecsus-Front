5edb7a7006fb8a7da0480818b0dd2bb63a8a52f9
wallaceHS20 <wallacehonorato67@gmail.com>
2024-05-02 00:08:36 -0300

    variacoes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,13 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 
 import Login from "../pages/Login/";
 import Home from "../pages/Home"
-import Dashboard from "../pages/Dashboard"
+import DashboardAgua from "../pages/DashboardAgua"
 
 function RoutesApp(){
     return(
         <Routes>
             <Route path='/home' element={<Home/>}/>
-            <Route path='/dashboard' element={<Dashboard/>}/>
+            <Route path='/dashboardAgua' element={<DashboardAgua/>}/>
             <Route path='/' element={<Login/>}/>
         </Routes>
     )
