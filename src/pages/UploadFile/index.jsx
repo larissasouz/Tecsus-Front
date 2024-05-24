b6b170bcb28f5903d7c3f2e033ff3a505c0fd1ef
Noite <wallacehonorato67@gmail.com>
2024-05-23 21:16:35 -0300

    feat: BT-71 - tela de upload de arquivos

--- src/pages/UploadFile/index.jsx
+++ src/pages/UploadFile/index.jsx
@@ -1,27 +1,61 @@
 import React, { useState } from 'react';
 import CSVReader from 'react-csv-reader';
-import './upload.css'
+import axios from 'axios';
+import { toast } from "react-toastify";
+import './upload.css';
 
-import Sidebar from '../../components/sidebar/sidebar'
-import Title from '../../components/title/title'
+import Sidebar from '../../components/sidebar/sidebar';
+import Title from '../../components/title/title';
 import { GrDocumentCsv } from "react-icons/gr";
 
 export default function UploadFile() {
 
     const [csvData, setCsvData] = useState(null);
-    const [fileActivate, setFileActivate] = useState(false)
+    const [fileInfo, setFileInfo] = useState(null);
+    const [fileActivate, setFileActivate] = useState(null);
+    const [typeFile, setTypeFile] = useState('con_agua.csv')
+
+    async function upload() {
+        if (!csvData || !fileInfo) {
+            alert('Por favor, carregue um arquivo antes de fazer o upload.');
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('file', new Blob([csvData], { type: 'text/csv' }), fileInfo.name);
+
+        await axios.post('http://127.0.0.1:8000/upload', formData)
+
+            .then(function (response) {
+                toast.success('Atualizado com sucesso!')
+            })
+
+            .catch(function (error) {
+                toast.error('Falha ao executar a atualização!')
+            });
+    }
+
+    function handleSelectChange(e) {
+        setTypeFile(e.target.value)
+    }
 
     const handleFileUpload = (data, fileInfo) => {
+        try {
 
-        try{
-            setCsvData({ data, fileInfo });
-            setFileActivate(true)
-        }
-        catch(e){
-            setFileActivate(false)
-        }
+            setCsvData(data);
+            setFileInfo(fileInfo);
 
-        
+            if(fileInfo.name !== typeFile){
+                toast.error('Selecione o arquivo correto!')
+                setFileActivate(false)
+                return
+            }
+
+            setFileActivate(true);
+
+        } catch (e) {
+            setFileActivate(false);
+        }
     };
 
     return (
@@ -31,20 +65,31 @@ export default function UploadFile() {
                 <Title name={"Atualizar Dados"}>
                     <GrDocumentCsv size={25} />
                 </Title>
-                <form className='form-upload'>
+                <form className='form-upload' onSubmit={(e) => { e.preventDefault(); upload(); }}>
+                    <div className='group-select'>
+                        <h4>Selecione o tipo</h4>
+                        <select value={typeFile} onChange={handleSelectChange}>
+                            <option value={"con_agua.csv"}>con_agua</option>
+                            <option value={"pro_agua.csv"}>pro_agua</option>
+                            <option value={"con_energia.csv"}>con_energia</option>
+                            <option value={"pro_energia.csv"}>pro_energia</option>
+                        </select>
+                    </div>
                     <div className='group-input'>
                         <label>Selecione o arquivo atualizado</label>
                         <div className='input-box'>
-                            {csvData ? <h2 className='arq-name'>Arquivo carregado: {csvData.fileInfo.name}</h2> : <h2 className='arq-name'></h2>}
-                            <CSVReader className={'button-csv'}
-                                onFileLoaded={handleFileUpload} />
+                            {fileInfo ? <h2 className='arq-name'>Arquivo carregado: {fileInfo.name}</h2> : <h2 className='arq-name'></h2>}
+                            <CSVReader
+                                cssClass={'button-csv'}
+                                onFileLoaded={handleFileUpload}
+                            />
                         </div>
-                        {fileActivate === true  && <span style={{ backgroundColor: '#226c22' }} className='label-msg' >Arquivo carregado com sucesso</span>}
-                        {fileActivate === null  && <span style={{ backgroundColor: '#930000' }} className='label-msg' >Falha ao carregar o arquivo</span>}
-                        
+                        {fileActivate === true && <span style={{ backgroundColor: '#226c22' }} className='label-msg'>Arquivo carregado com sucesso</span>}
+                        {fileActivate === false && <span style={{ backgroundColor: '#930000' }} className='label-msg'>Falha ao carregar o arquivo</span>}
+                        <button type="submit" className='upload-button'>Upload</button>
                     </div>
                 </form>
             </main>
         </div>
-    )
+    );
 }
